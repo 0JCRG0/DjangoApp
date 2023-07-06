a043b0f9ec13fc3fa4e0088379bb92b19540d95a
JCRG <juancarlosrg1999@gmail.com>
2023-07-05 23:22:35 -0600

    summarise a cv

--- DreamedJobAI/views.py
+++ DreamedJobAI/views.py
@@ -7,6 +7,7 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from django.core.files.base import ContentFile
 import magic
+from .utils import summarise_cv
 
 def index(request):
     if request.method == 'POST':
@@ -15,9 +16,11 @@ def index(request):
             user_text = form.save(commit=False)
             pdf_file = request.FILES['pdf_file']
             user_text.pdf_file = pdf_file
-            user_text.extracted_text = extract_text_from_pdf(pdf_file)
+            extracted_text = extract_text_from_pdf(pdf_file)
+            user_text.extracted_text = extracted_text
             user_text.save()
-            return redirect('DreamedJobAI:index')
+            summarised_cv = summarise_cv(extracted_text)
+            return render(request, 'DreamedJobAI/summary.html', {'summarised_cv': summarised_cv})
     else:
         form = UploadPDFForm()
     return render(request, 'DreamedJobAI/index.html', {'form': form})
