1939d20659e3840c9235f7253af0d832f0cbfcb6
JCRG <juancarlosrg1999@gmail.com>
2023-08-09 17:09:42 -0600

    fixed form

--- DreamedJobAI/models.py
+++ DreamedJobAI/models.py
@@ -5,14 +5,16 @@ from django.contrib.auth.models import User
 # PDF MODEL---------------------------/
 #-----------------------------------------------/
 
-class UserText(models.Model):
+
+class UserCV(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     pdf_file = models.FileField(upload_to='pdf_files/', null=True)
     extracted_text = models.TextField(null=True)
 
     def __str__(self):
-        return self.text
+        return self.user.username
     
 # ----------------------------------------------/
 # Email is unique now---------------------------/
