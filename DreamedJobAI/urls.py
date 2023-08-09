d5e74176635b989b4e3857475a85016aa06f8490
JCRG <juancarlosrg1999@gmail.com>
2023-08-09 17:09:33 -0600

    fixed form

--- DreamedJobAI/urls.py
+++ DreamedJobAI/urls.py
@@ -15,7 +15,7 @@ from django.contrib.auth.views import (
 app_name = 'DreamedJobAI'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('submit_pdf/', views.submit_pdf, name='submit_pdf'),
+    #path('submit_pdf/', views.submit_pdf, name='submit_pdf'),
     path('login/', MyLoginView.as_view(template_name='DreamedJobAI/registration/login.html'),name='login'),
     path('logout/', LogoutView.as_view(next_page="/"),name='logout'),
     path('register/', RegisterView.as_view(),name='register'),
