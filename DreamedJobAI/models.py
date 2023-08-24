bb948569b7dc46c903a2325174bcab60200e0976
JCRG <juancarlosrg1999@gmail.com>
2023-08-24 14:30:46 -0600

    test

--- DreamedJobAI/models.py
+++ DreamedJobAI/models.py
@@ -10,8 +10,9 @@ class UserCV(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    pdf_file = models.FileField(upload_to='pdf_files/', null=True)
+    pdf_file = models.FileField(upload_to='pdf_files/', null=False)
     extracted_text = models.TextField(null=True)
+    summary = models.TextField(null=True)
 
     def __str__(self):
         return self.user.username
